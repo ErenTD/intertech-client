546bc03b42f55dc0acef6cbf212841d3a93e2a54
Ezgi Başak Mert <ezgibasakmert@Ezgi-MacBook-Pro.local>
2022-08-19 12:13:39 +0300

    Added the Add Child Page

---
+++ src/pages/AddChildPage.js
@@ -0,0 +1,69 @@
+import {
+    Button,
+    DatePicker,
+    Form,
+    Input,
+    InputNumber,
+    Card,
+    Row
+  } from 'antd';
+  import React, { useState } from 'react';
+  import "antd/dist/antd.min.css";
+  
+  const AddChildPage = () => {
+    const [componentSize, setComponentSize] = useState('default');
+  
+    const onFormLayoutChange = ({ size }) => {
+       setComponentSize(size);
+    };
+  
+    return (
+       <Row type='flex' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10%' }}>
+          <Card
+             title="Yeni Çocuk Ekle"
+             style={{
+                width: 600,
+                verticalAlign: 'middle'
+             }}
+          >
+             <Form
+                labelCol={{
+                   span: 8,
+                }}
+                wrapperCol={{
+                   span: 16,
+                }}
+  
+                initialValues={{
+                   size: componentSize,
+                }}
+                onValuesChange={onFormLayoutChange}
+                size={componentSize}
+             >
+                <Form.Item
+                   label="İsim/Soyisim">
+                   <Input style={inputStyle} />
+                </Form.Item>
+                <Form.Item label="Cüzdan Adresi">
+                   <Input style={inputStyle} />
+                </Form.Item>
+                <Form.Item label="Doğum Günü">
+                   <DatePicker style={inputStyle} />
+                </Form.Item>
+                <Form.Item label="Eth Miktarı">
+                   <InputNumber style={inputStyle} />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                   <Button type='primary'>Kaydet</Button>
+                </Form.Item>
+             </Form>
+          </Card>
+       </Row>
+    );
+  };
+  
+  const inputStyle = {
+    width: '300px'
+  };
+  
+  export default AddChildPage;
